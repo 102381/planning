9313054658b2e6ac65b56ae50dc0c975630d261d
Zero <102381@glr.nl>
2026-03-24 10:25:14 +0000

    fixed some issues

--- config.php
+++ config.php
@@ -2,11 +2,25 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+
+$envFile = __DIR__ . '/planning/.env';
+if (file_exists($envFile)) {
+    $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos($line, '=') !== false && strpos($line, '#') !== 0) {
+            list($key, $value) = explode('=', $line, 2);
+            $key = trim($key);
+            $value = trim($value);
+            putenv("$key=$value");
+        }
+    }
+}
+
 $servername = 'localhost';
 $port = '3306';
-$username = 'blink_102381';
-$password = 'blink_102381';
-$dbname = 'blink_102381';
+$username = getenv('USERNAME');
+$password = getenv('PASSWORD');
+$dbname = getenv('DBNAME');
 
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
